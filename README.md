8de994b977aea366f413b3f80f08f77a21c49079
MrLordBrown <mrlordvondoombraun@gmail.com>
2021-05-11 00:26:05 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# web-platform-ogdckb
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/web-platform-ogdckb)
---
+++ script.js
@@ -0,0 +1,10 @@
+var enter = document.getElementById("enter");
+enter.addEventListener("click",tazCheck());
+
+function tazCheck(){
+  var numz = document.getElementById("inputz").nodeValue;
+  console.log(numz);
+  for (var i = 0; i < numz;i++) {
+    var target = document.getElementById("dataz");
+    
+  }}
