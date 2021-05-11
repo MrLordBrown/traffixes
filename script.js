08b80b40624138b35fdc6f4482d4e55e06a247e5
MrLordBrown <mrlordvondoombraun@gmail.com>
2021-05-11 00:52:31 -0700

    1 file modified

--- script.js
+++ script.js
@@ -6,5 +6,18 @@ function tazCheck(){
   console.log(numz);
   for (var i = 0; i < numz;i++) {
     var target = document.getElementById("dataz");
-    
-  }}
+    var taz = document.createElement(
+      "section");
+      var tazNum = document.createElement("div");
+      var typez = document.createElement("input");
+      var amount = document.createElement("input");
+      var qty = document.createElement("input");
+  var rate = document.createElement("input");
+  var init = document.createElement("input");
+  var outit = document.createElement("input");
+  var totalz = document.createElement("div");
+  var production = document.createElement("div");
+ var attraction = document.createElement("div");
+ tazNum.textContent = i;
+ 
+   }}
